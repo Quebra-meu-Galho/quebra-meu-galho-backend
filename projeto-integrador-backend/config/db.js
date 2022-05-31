725f2dd496b9ff555f6a2fea157e660d9ae9fce4
Marcos Rampaso <49240709+Mazner@users.noreply.github.com>
2022-05-30 21:18:19 -0300

    Add files via upload

--- projeto-integrador-backend/config/db.js
+++ projeto-integrador-backend/config/db.js
@@ -1,3 +1,10 @@
+/* Comandos para rodar o postgree no cmd:
+1º- psql -U postgres
+2º- \l para listar os DBs
+3º- \c quebra_meu_galho para se conectar ao DB do app
+4º- \dt para listar as tabelas
+*/
+
 const config = require ('../knexfile.js');
 const knex = require ('knex')(config); //puxando o knex junto com a configuração
 
